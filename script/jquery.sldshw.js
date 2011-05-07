8274f890b3dacbd5b9a75f81ba61d3b11fbe3b33
Kelvin Luck <git@kelvinluck.com>
2011-05-07 09:13:30 +0100

    Adding image tags to the document for the loaded photos and sizing them correctly to fit into their container...

--- script/jquery.sldshw.js
+++ script/jquery.sldshw.js
@@ -18,11 +18,19 @@
 						api_key: s.apiKey,
 						format: 'json',
 						//nojsoncallback: 1,
-						extras: settings.imageSize
-					};
-				// TODO: Clean up previous one?
+						extras: 'url_' + settings.imageSize
+					},
+					containerW = elem.width(),
+					containerH = elem.height(),
+					containerRatio = containerW / containerH,
+					ul = $('<ul/>').width(containerW).height(containerH);
+
+				elem.empty().append(
+					$('<div class="sldshow"/>').append(ul)
+				);
+
 				switch (settings.action) {
-					case 'showFavourites':
+					case 'favorites.getPublicList':
 						params.method = 'flickr.favorites.getPublicList';
 						params.user_id = settings.userId;
 						break;
@@ -39,11 +47,42 @@
 				).success(
 					function(data, status, xhr)
 					{
+						ul.width(data.photos.photo.length * containerW)
 						$.each(
 							data.photos.photo,
 							function(i, photo)
 							{
-								console.log(photo[settings.imageSize]);
+								var w = photo['width_' + settings.imageSize],
+									h = photo['height_' + settings.imageSize],
+									photoRatio = w / h,
+									destW, destH, destX, destY, scale;
+								if (settings.cropToContainer) {
+									if (photoRatio > containerRatio) {
+										scale = containerH / h;
+									} else {
+										scale = containerW / w;
+									}
+								} else {
+									if (photoRatio > containerRatio) {
+										scale = containerW / w;
+									} else {
+										scale = containerH / h;
+									}
+								}
+								w = Math.round(w * scale);
+								h = Math.round(h * scale);
+								ul.append(
+									$('<li style="width:' + containerW + 'px; height:' + containerH + 'px;"/>').append(
+										$('<img />')
+											.attr('src', photo['url_' + settings.imageSize])
+											.css(
+												{
+													width: w,
+													height: h
+												}
+											)
+									)
+								);
 							}
 						);
 					}
@@ -80,8 +119,18 @@
 		}
 
 		$.fn.sldshw.defaults = {
-			flickrApiEndpoint: 'http://api.flickr.com/services/rest/',
-			imageSize: 'url_m'
+			// The number of milliseconds to show each picture for
+			displayTime: 6000,
+			// The number of milliseconds to spend fading between each picture
+			fadeTime: 1000,
+			// Whether to crop the images to the size of the container (true) or to size them so that the fit within
+			// the bounds of the container (false)
+			cropToContainer: true,
+			// Which size image to load (it will be sized to fit in the container anyway) - one of:
+			// sq, t, s, m, z, l, o
+			imageSize: 'm',
+			// The endpoint on flickr.com to connect to - should never change...
+			flickrApiEndpoint: 'http://api.flickr.com/services/rest/'
 		};
 	}
 )(jQuery,this);
